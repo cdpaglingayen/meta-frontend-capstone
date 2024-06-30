54216ac2e976fcb9b852e1e73bc772f4c2fd5d1a
Charles Danielle Paglingayen <65948329+cdpaglingayen@users.noreply.github.com>
2024-06-30 08:54:59 +0800

    Added OG Meta Tags and Initial Components

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Nav = () => {
+  return (
+    <>
+      <section>
+        <img src='./src/assets/logo.svg' />
+      </section>
+      <ul>
+        <li>
+          <a href=''></a>
+        </li>
+        <li>
+          <a href=''></a>
+        </li>
+        <li>
+          <a href=''></a>
+        </li>
+        <li>
+          <a href=''></a>
+        </li>
+        <li>
+          <a href=''></a>
+        </li>
+      </ul>
+    </>
+  );
+};
