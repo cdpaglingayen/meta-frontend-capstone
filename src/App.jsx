132357bc6b1daf6d3189354c59b5654bb1dce93d
Charles Danielle Paglingayen <65948329+cdpaglingayen@users.noreply.github.com>
2024-07-01 10:21:42 +0800

    Revising file names and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
+import { HomePage } from './components/HomePage';
 import { Navigation } from './components/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,9 +7,7 @@ function App() {
   return (
     <>
       <Navigation />
-      <Header />
-      <Main />
-      <Footer />
+      <HomePage />
     </>
   );
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,19 @@
+import Chicago from './Chicago';
+import { Footer } from './Footer';
+import { Header } from './Header';
+import Specials from './Specials';
+import Testimonials from './Testimonials';
+
+export const HomePage = () => {
+  return (
+    <>
+      <main>
+        <Header />
+        <Specials />
+        <Testimonials />
+        <Chicago />
+        <Footer />
+      </main>
+    </>
+  );
+};
